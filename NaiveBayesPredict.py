fa2553f127e54b866953db18b8355e05efbfb290
JonNixonCodes <jonathan.yu.94@gmail.com>
2020-07-11 14:29:11 +1000

    Implemented cross-validation and categorical NB

--- NaiveBayesPredict.py
+++ NaiveBayesPredict.py
@@ -7,40 +7,65 @@ Created on Mon Jul  6 18:06:25 2020
 @author: jonat
 """
 # %% Load libraries
+import tqdm
 import pandas as pd
 import numpy as np
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import LeaveOneGroupOut
+from sklearn.naive_bayes import CategoricalNB
 import FootballDataAnalysis as fda
 
 # %% Load data
 fd = fda.LoadFootballData()
     
-# %% Assign train and test splits
+# %% Assign train/validation and test splits
 train_seasons = ['1718','1617','1516','1415','1314','1213','1112','1011','0910']
 test_seasons = ['1819']
-fd_train = fd[fd['Season'].isin(train_seasons)]
-fd_test = fd[fd['Season'].isin(test_seasons)]
+train_index = fd['Season'].isin(train_seasons)
+test_index = fd['Season'].isin(test_seasons)
 
 # %% Extract labels and features
-X_train = fda.ExtractPrevResults(fd_train, form='HomeAway', n=3)
-X_test = fda.ExtractPrevResults(fd_test, form='HomeAway', n=3)
-y_train = fda.ExtractResult(fd_train, result='H')
-y_test = fda.ExtractResult(fd_test, result='H')
+X_prevResults = fda.ExtractPrevResults(fd, form='HomeAway', n=3)
+X_homeTeam = fd['HomeTeam']
+X_awayTeam = fd['AwayTeam']
+y = fda.ExtractResult(fd, result='H')
 
 # %% Pre-process (encoding categories)
-X_train_enc = fda.EncodeCategories(X_train.flatten(), categories=['W','L','D']).reshape(X_train.shape)
-X_test_enc = fda.EncodeCategories(X_test.flatten(), categories=['W','L','D']).reshape(X_test.shape)
-y_train_enc = fda.EncodeCategories(y_train, categories=['H','O'])
-y_test_enc = fda.EncodeCategories(y_test, categories=['H','O'])
+teams = list(np.unique(X_homeTeam))
+results = ['W','L','D']
+labels = ['H','O']
+X_prevResults = fda.EncodeCategories(X_prevResults.flatten(), 
+                                     categories=results).reshape(X_prevResults.shape)
+X_homeTeam = fda.EncodeCategories(X_homeTeam, categories=teams)\
+    .reshape(X_homeTeam.shape)
+X_awayTeam = fda.EncodeCategories(X_awayTeam, categories=teams)\
+    .reshape(X_awayTeam.shape)
+X = np.concatenate([X_prevResults.flatten(),X_homeTeam,X_awayTeam])\
+    .reshape(X_prevResults.shape[1]+2,X_prevResults.shape[0]).transpose()
+y = fda.EncodeCategories(y, categories=labels)
 
+# %% Cross-validation (Leave-one-group-out)
+X_cv, y_cv = X[train_index], y[train_index]
+cv_groups = fda.EncodeCategories(fd[train_index]['Season'], train_seasons)
+cv_accuracy = []
+logo = LeaveOneGroupOut()
+for cv_train_index, cv_test_index in tqdm.tqdm(logo.split(X_cv, y_cv, groups=cv_groups), 
+                                               total=len(np.unique(cv_groups))):
+    X_cvtrain, X_cvtest = X_cv[cv_train_index], X_cv[cv_test_index]
+    y_cvtrain, y_cvtest = y_cv[cv_train_index], y_cv[cv_test_index]
+    model = CategoricalNB().fit(X_cvtrain, y_cvtrain)
+    pred = model.predict(X_cvtest)
+    cv_accuracy.append(np.mean(pred==y_cvtest))
 
-# %% Train Naive Bayes Model
-model = MultinomialNB().fit(X_train_enc, y_train_enc)
+# %% Train Naive Bayes model
+X_train, y_train = X[train_index], y[train_index]
+X_test, y_test = X[test_index], y[test_index]
+model = CategoricalNB().fit(X_train, y_train)
 
-# %% Test Naive Bayes Model
-predicted = model.predict(X_test_enc)
-print(f"model accuracy = {np.mean(predicted == y_test_enc)}")
+# %% Test Naive Bayes model
+pred = model.predict(X_test)
+print(f"model accuracy = {np.mean(pred==y_test)}")
 
 # %% Analyse results
-predicted = fda.DecodeCategories(predicted, categories=['H','O'])
-result_df = pd.DataFrame(np.concatenate([np.transpose(X_test).flatten(),predicted,y_test]).reshape(8,380).transpose())
+pred = fda.DecodeCategories(pred, categories=labels)
+y_test = fda.DecodeCategories(y_test, categories=labels)
+result_df = pd.DataFrame(np.concatenate([np.transpose(X_test).flatten(),pred,y_test]).reshape(10,380).transpose())
